8a291e2392dc57c7c180f58ca8948bb6908685df
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-04-08 14:21:06 +0100

    Registers TableDisplayPanel with RoomManagerUI

--- src/manager/RoomManagerUI.java
+++ src/manager/RoomManagerUI.java
@@ -2,22 +2,28 @@ package manager;
 
 import javax.swing.*;
 import java.awt.*;
+
+import sharedui.TableDisplayPanel;
 import sharedui.TableSearchPanel;
 
 public class RoomManagerUI extends JPanel {
 
     private static RoomManagerUI instance;
     private TableSearchPanel searchPanel;
+    private TableDisplayPanel tableDisplayPanel;
 
     private RoomManagerUI(){
         //Drawing component implementation goes here
         setLayout(new FlowLayout());
 
+        //Initialise objects
         initUI();
 
-        //Add components to UI
+        //Add search panel to UI
         add(searchPanel);
 
+        //Add table display panel to UI
+        add(tableDisplayPanel);
 
     }
 
@@ -29,9 +35,9 @@ public class RoomManagerUI extends JPanel {
     }
 
     private void initUI() {
-        if (searchPanel == null) {
-            searchPanel = new TableSearchPanel();
-        }
+        if (searchPanel == null) { searchPanel = new TableSearchPanel(); }
+        if (tableDisplayPanel == null) { tableDisplayPanel = new TableDisplayPanel(); }
+
     }
 
 
