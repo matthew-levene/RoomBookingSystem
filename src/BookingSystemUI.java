f624f0affb9dcaa2d735b6743cfa92e28503abc0
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-04-08 11:41:53 +0100

    Implements frame to anchor user interfaces

---
+++ src/BookingSystemUI.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class BookingSystemUI extends JPanel{
+    public BookingSystemUI(){
+        //Set the layout
+        setLayout(new BorderLayout());
+
+    }
+    public static void main(String[] args){
+        //Setup the frame for the application
+       JFrame frame = new JFrame("Room Booking System");
+       frame.setSize(400,400);
+       frame.setLocationRelativeTo(null);
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.add(new BookingSystemUI());
+       frame.setVisible(true);
+    }
+}
