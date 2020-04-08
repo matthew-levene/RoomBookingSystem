0328941077f0b5b158e69ae883ddba002e20854f
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-04-08 13:28:38 +0100

    Adds room manager user interface to BookingSystemUI

--- src/BookingSystemUI.java
+++ src/BookingSystemUI.java
@@ -1,3 +1,5 @@
+import manager.RoomManagerUI;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -5,6 +7,9 @@ public class BookingSystemUI extends JPanel{
     public BookingSystemUI(){
         //Set the layout
         setLayout(new BorderLayout());
+
+        RoomManagerUI managerUI = RoomManagerUI.getInstance();
+        add(managerUI, BorderLayout.NORTH);
 
     }
     public static void main(String[] args){
