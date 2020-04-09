92d874bd047756ed8bc8a84ad38e098ed1f3d327
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-04-09 14:44:11 +0100

    Updates SharedRooms class with getter, setter and removal methods

---
+++ src/shared/data/SharedRooms.java
@@ -0,0 +1,45 @@
+package shared.data;
+
+import java.util.HashMap;
+import java.util.Observable;
+
+public class SharedRooms extends Observable {
+    private static SharedRooms instance;
+    private HashMap<String, Room> sharedRooms;
+
+    private SharedRooms(){
+        sharedRooms = new HashMap<>();
+    }
+
+    public static synchronized SharedRooms getInstance(){
+        if(instance == null){ instance = new SharedRooms(); }
+        return instance;
+    }
+
+    public boolean keyExists(String key){
+        return sharedRooms.containsKey(key);
+    }
+
+    public void addRoom(String key, Room value){
+        sharedRooms.put(key, value);
+
+        //TODO Notify observers new room has been added
+    }
+
+    public void removeRoom(String key){
+        if(sharedRooms.containsKey(key)){
+            sharedRooms.remove(key);
+        }
+
+        //TODO Notify observers that room has been removed
+    }
+
+    public synchronized Room getRoom(String key){
+        if(sharedRooms.containsKey(key)){
+            return sharedRooms.get(key);
+        }
+        return null;
+    }
+
+
+}
