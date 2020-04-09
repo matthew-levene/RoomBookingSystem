ceb8392317226fc78ad2ad633d90ccbb166608e2
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-04-09 16:27:36 +0100

    Removes redundant code from the 'add' method call

--- src/shared/ui/TableDisplayPanel.java
+++ src/shared/ui/TableDisplayPanel.java
@@ -7,11 +7,9 @@ import java.awt.*;
 
 public class TableDisplayPanel extends JPanel {
 
-    private JTabbedPane tabbedPane;
     private JTable roomTable;
     private DefaultTableModel tableModel;
 
-    private GridBagConstraints cons;
     public TableDisplayPanel(){
 
         String[] tableHeaders = {"Name", "Type", "Capacity", "Availability"};
@@ -21,10 +19,9 @@ public class TableDisplayPanel extends JPanel {
             public boolean isCellEditable(int row, int column) { return false; }
         };
 
-        //roomTable.setPreferredSize(new Dimension(200,50));
         JScrollPane scrollPane = new JScrollPane(roomTable);
         scrollPane.setPreferredSize(new Dimension(280, 124));
-        add(scrollPane, cons);
+        add(scrollPane);
 
     }
 
