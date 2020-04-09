ccec00b7934156cf5fb8dca89e0213e70be23944
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-04-09 14:33:22 +0100

    Updates room to include assignment and retrieval of availability array

--- src/shared/data/Room.java
+++ src/shared/data/Room.java
@@ -1,13 +1,17 @@
 package shared.data;
 
+import java.util.ArrayList;
+
 public class Room {
 
     private String roomName, roomType, roomCapacity;
+    private ArrayList<Availability> availabilities;
 
-    public Room(String name, String type, String capacity){
+    public Room(String name, String type, String capacity, ArrayList<Availability> availabilities){
         roomName = name;
         roomType = type;
         roomCapacity = capacity;
+        this.availabilities = availabilities;
     }
 
     public String getRoomName(){
@@ -21,4 +25,6 @@ public class Room {
     public String getRoomCapacity(){
         return roomCapacity;
     }
+
+    public ArrayList<Availability> getAvailabilities() {return availabilities; }
 }
