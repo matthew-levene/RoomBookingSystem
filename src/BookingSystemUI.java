d3294521eec42350ade2857f3e1afa13cb84c418
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-04-09 16:23:09 +0100

    Adds RoomManagerUI to its own dedicated thread

--- src/BookingSystemUI.java
+++ src/BookingSystemUI.java
@@ -1,24 +1,28 @@
 import manager.RoomManagerUI;
+import manager.dialogs.AddRoomDialog;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class BookingSystemUI extends JPanel{
-    public BookingSystemUI(){
+public class BookingSystemUI extends JPanel {
+    public BookingSystemUI() {
         //Set the layout
         setLayout(new BorderLayout());
 
-        RoomManagerUI managerUI = RoomManagerUI.getInstance();
-        add(managerUI, BorderLayout.NORTH);
-
+        Thread rmThread = new Thread(() -> {
+            RoomManagerUI managerUI = RoomManagerUI.getInstance();
+            add(managerUI, BorderLayout.NORTH);
+        });
+        rmThread.start();
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         //Setup the frame for the application
-       JFrame frame = new JFrame("Room Booking System");
-       frame.setSize(950,200);
-       frame.setLocationRelativeTo(null);
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       frame.add(new BookingSystemUI());
-       frame.setVisible(true);
+        JFrame frame = new JFrame("Room Booking System");
+        frame.setSize(950, 200);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new BookingSystemUI());
+        frame.setVisible(true);
     }
 }
