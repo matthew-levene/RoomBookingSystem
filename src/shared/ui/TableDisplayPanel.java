00308cca08ebafd5b9b026dd5e778530f9d541c8
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-04-10 18:16:44 +0100

    Adds a second table with getter and setter methods to model unavailable rooms

--- src/shared/ui/TableDisplayPanel.java
+++ src/shared/ui/TableDisplayPanel.java
@@ -7,48 +7,58 @@ import java.awt.*;
 
 public class TableDisplayPanel extends JPanel {
 
-    private JTable roomTable;
-    private DefaultTableModel tableModel;
+    private JTable availRoomTable, unavailRoomTable;
+    private DefaultTableModel availTableModel, unavailTableModel;
+    private JTabbedPane tabbedPane;
 
     public TableDisplayPanel(){
-
-        String[] tableHeaders = {"Name", "Type", "Capacity", "Availability"};
-        tableModel = new DefaultTableModel(tableHeaders, 0);
-
-        roomTable = new JTable(tableModel){
+        String[] availHeaders = {"Name", "Type", "Size", "Availability"};
+        //Define the available rooms table
+        availTableModel = new DefaultTableModel(availHeaders, 0);
+        availRoomTable = new JTable(availTableModel){
             public boolean isCellEditable(int row, int column) { return false; }
         };
+        JScrollPane scrollPane1 = new JScrollPane(availRoomTable);
+        scrollPane1.setPreferredSize(new Dimension(280, 100));
 
-        JScrollPane scrollPane = new JScrollPane(roomTable);
-        scrollPane.setPreferredSize(new Dimension(280, 124));
-        add(scrollPane);
+        //Define the unavailable rooms table
+        String[] unavailHeaders = {"Name", "Reason", "Timescale"};
+        unavailTableModel = new DefaultTableModel(unavailHeaders, 0);
+        unavailRoomTable = new JTable(unavailTableModel){
+            public boolean isCellEditable(int row, int column) { return false; }
+        };
+        JScrollPane scrollPane2 = new JScrollPane(unavailRoomTable);
+        scrollPane2.setPreferredSize(new Dimension(280, 100));
+
+        //Add the two tables to the tabbed pane
+        tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("Available", scrollPane1);
+        tabbedPane.addTab("Unavailable", scrollPane2);
+        add(tabbedPane);
     }
 
-    public void addRow(Object[] rowData){
-        tableModel.addRow(rowData);
-    }
+    public void addRow(Object[] rowData){ availTableModel.addRow(rowData); }
+    public void addUnavailRow(Object[] rowData){ unavailTableModel.addRow(rowData); }
 
-    public int getSelectedRow(){
-        //Check if row is selected
-        return roomTable.getSelectedRow();
-    }
+    public void removeRow(int index){ availTableModel.removeRow(index); }
+    public void removeUnavailRow(int index){ unavailTableModel.removeRow(index); }
 
-    public Object getRoomName(int row){
-        return  tableModel.getValueAt(row, 0);
-    }
+    public int getSelectedRow(){ return availRoomTable.getSelectedRow(); }
+    public int getSelectedUnavailRow(){ return unavailRoomTable.getSelectedRow(); }
 
-    public void removeRow(int index){
-        tableModel.removeRow(index);
+    public Object getRoomName(int row){ return  availTableModel.getValueAt(row, 0); }
+    public Object getUnavailRoomName(int row){
+        return unavailTableModel.getValueAt(row, 0);
     }
 
     public int getRowCount(){
-        return tableModel.getRowCount();
+        return availTableModel.getRowCount();
+    }
+    public int getUnavailRowCount(){
+        return unavailTableModel.getRowCount();
     }
 
-
-    public void clearTable(){
-        tableModel.setRowCount(0);
+    public String getTabTitle(){
+        return tabbedPane.getTitleAt(tabbedPane.getSelectedIndex());
     }
-
-
 }
