2a286c2ebb67b4e66b0828f8b610ef5db7b62739
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-04-11 12:50:01 +0100

    Adds TermDatesDialog to get term dates input from room manager

---
+++ src/manager/dialogs/TermDatesDialog.java
@@ -0,0 +1,108 @@
+package manager.dialogs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TermDatesDialog extends JDialog implements ActionListener {
+
+    private JLabel firstTermLbl, scndTermLbl, startLbl1, startLbl2, endLbl1, endLbl2;
+    private JTextField firstStartTxt, firstEndTxt, scndStartTxt, scndEndTxt;
+    private JButton submitBtn, cancelBtn;
+
+    public TermDatesDialog(){
+        super(new JDialog(), "Set Term Dates Dialog", true);
+        setSize(370,200);
+        setLocationRelativeTo(null);
+
+        initUI();
+        drawUI();
+
+        setVisible(true);
+
+    }
+
+    public void initUI() {
+        firstTermLbl = new JLabel("Term 1");
+        startLbl1 = new JLabel("Start: ");
+        endLbl1 = new JLabel("End: ");
+        scndTermLbl = new JLabel("Term 2");
+        startLbl2 = new JLabel("Start: ");
+        endLbl2 = new JLabel("End: ");
+
+        firstStartTxt = new JTextField(5);
+        firstEndTxt = new JTextField(5);
+        scndStartTxt = new JTextField(5);
+        scndEndTxt = new JTextField(5);
+
+        submitBtn = new JButton("Submit");
+        submitBtn.addActionListener(this);
+        cancelBtn = new JButton("Cancel");
+        cancelBtn.addActionListener(this);
+    }
+
+    public void drawUI() {
+        setLayout(new GridBagLayout());
+        GridBagConstraints cons = new GridBagConstraints();
+        cons.weightx = 0.1;
+        cons.weighty = 0.1;
+        cons.insets = new Insets(5,5,5,5);
+
+        //Add the Term 1 (T1) label
+        cons.gridx = 0;
+        cons.gridy = 0;
+        add(firstTermLbl, cons);
+
+        //Add T1 start label
+        cons.gridx = 0;
+        cons.gridy = 1;
+        add(startLbl1, cons);
+        //Add T1 start text field
+        cons.gridx = 1;
+        add(firstStartTxt, cons);
+        //Add T1 end label
+        cons.gridx = 2;
+        add(endLbl1, cons);
+        //Add T1 end text field
+        cons.gridx = 3;
+        add(firstEndTxt, cons);
+
+        //Add the Term 2 (T2) label
+        cons.gridx = 0;
+        cons.gridy = 2;
+        add(scndTermLbl, cons);
+
+        //Add T2 start label
+        cons.gridx = 0;
+        cons.gridy = 3;
+        add(startLbl2, cons);
+        //Add T2 start text field
+        cons.gridx = 1;
+        add(scndStartTxt, cons);
+        //Add T2 end label
+        cons.gridx = 2;
+        add(endLbl2, cons);
+        //Add T2 end text field
+        cons.gridx = 3;
+        add(scndEndTxt, cons);
+
+        //Add the submit and cancel buttons
+        cons.gridwidth = 3;
+        cons.anchor = GridBagConstraints.CENTER;
+        cons.insets = new Insets(20,5,5,5);
+
+        cons.gridx = 0;
+        cons.gridy = 4;
+        add(submitBtn, cons);
+
+        cons.gridx = 1;
+        add(cancelBtn, cons);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+}
