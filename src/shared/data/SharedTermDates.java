e888f583c1077dad47be7bed2b3b1f2fa3342fb0
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-04-11 14:54:00 +0100

    Adds SharedTermDates singleton class as a queue-based data structure for term date objects

---
+++ src/shared/data/SharedTermDates.java
@@ -0,0 +1,38 @@
+package shared.data;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SharedTermDates {
+    private static SharedTermDates instance;
+    private Queue<TermDate> sharedTermDates;
+
+    private SharedTermDates(){
+        //Polymorphic assignment to Queue using linked list
+        // (which implements, Queue and List Collection interfaces)
+        //Shared term dates can now 'behave' as a Queue.
+        sharedTermDates = new LinkedList<>();
+    }
+    public static SharedTermDates getInstance(){
+        if(instance == null){ instance = new SharedTermDates();}
+        return instance;
+    }
+
+    public void addTermDate(TermDate termDate){
+        //Add a term date object to the end of the queeu
+        sharedTermDates.add(termDate);
+    }
+
+    public synchronized TermDate peekAtTermDate(){
+        //Return the first element at the head of the queue.
+        return sharedTermDates.peek();
+    }
+
+    //Implemented but never called
+    //Prototype does not call for term dates to be deleted
+    private void removeHead(){
+        //Remove the first element at the head of the queue.
+        sharedTermDates.remove();
+    }
+}
